951e14d1da2b73cdcbf8067a14022432e786c637
Yuvraj Kishor <77017763+Kishoryuvi@users.noreply.github.com>
2022-02-01 09:54:40 +0530

    Create middleOfTheLinkedlist.cpp

---
+++ middleOfTheLinkedlist.cpp
@@ -0,0 +1,25 @@
+ListNode* middleNode(ListNode* head) {
+        
+        if(head == NULL){
+            
+              return head;
+        }
+          
+        
+        ListNode* slow = head;
+        ListNode* fast = head -> next;
+        
+        while(fast != NULL){
+            fast = fast -> next;
+            if(fast != NULL){
+                fast = fast -> next;
+            }
+            
+            slow = slow -> next;
+            
+        }
+        
+        return slow;
+        
+    }
+};
